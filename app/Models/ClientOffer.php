4e3bae48dbdbe32c3b81074145140819c102e9c3
Muhammet95 <go.suits.3@gmail.com>
2022-09-20 01:48:46 +0300

    added client_offers model with migrations

--- app/Models/ClientOffer.php
+++ app/Models/ClientOffer.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class ClientOffer extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_name',
+        'email',
+        'from',
+        'to',
+        'original_filepath',
+        'status',
+        'template_filepath',
+        'translate_filepath',
+        'word_count',
+        'price',
+        'tariff',
+        'translator_name',
+        'user_comment',
+        'translator_comment'
+    ];
 }
--- database/migrations/2022_09_19_222919_create_client_offers_table.php
+++ database/migrations/2022_09_19_222919_create_client_offers_table.php
@@ -15,6 +15,20 @@ return new class extends Migration
     {
         Schema::create('client_offers', function (Blueprint $table) {
             $table->id();
+            $table->text('user_name')->nullable();
+            $table->string('email', 128)->unique();
+            $table->string('from', 16)->default('english');
+            $table->string('to', 16)->default('turkey');
+            $table->string('original_filepath', 512);
+            $table->string('status', 128);
+            $table->string('template_filepath', 512);
+            $table->string('translate_filepath', 512);
+            $table->integer('word_count')->default(0);
+            $table->float('price')->default(0.0);
+            $table->float('tariff')->default(0.03);
+            $table->text('translator_name')->nullable();
+            $table->text('user_comment')->nullable();
+            $table->text('translator_comment')->nullable();
             $table->timestamps();
         });
     }
